596a427daee0a7ea8d7d184c6c452a257236ad2e
Barkın Çeliker <barkinclkr@gmail.com>
2025-06-07 19:28:31 +0000

    panele gitmedi

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,17 +21,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - files in public folder (e.g. images, resume-placeholder.pdf)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)',
-    '/admin/:path*', // Explicitly include admin paths for clarity
-    '/login', // Explicitly include login path
-  ],
+  matcher: ['/admin/:path*', '/login'],
 };
