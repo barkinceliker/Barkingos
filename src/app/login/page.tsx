c5b680e57afb2f31283bfeabe8588efa51d65bc1
Barkın Çeliker <barkinclkr@gmail.com>
2025-06-08 14:07:54 +0000

    In the file src/app/login/page.tsx, at line 2, there is an error indicat

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
+// This page now re-exports the AdminLoginPage from its new location.
+// The /login route will render the content of /app/admin/login/page.tsx.
 
-This page has been moved to /app/admin/login/page.tsx
-Please remove this file if it's no longer needed or update references.
-If you were redirected here, the login functionality is now under the /admin path.
-You can try accessing /admin to be redirected to the new login page if not authenticated.
+import AdminLoginPage from '@/app/admin/login/page';
+
+export default AdminLoginPage;
